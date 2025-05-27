0362259598dfcfb207aa056cef782568a9e5330c
Mel4sa <melisaxsimsek@gmail.com>
2025-05-27 13:32:29 +0300

    buttons connections

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var firstNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func plusButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+    }
+   
+    
+    @IBAction func DividedBy(_ sender: UIButton) {
+    }
+    @IBAction func MultipliedBy(_ sender: UIButton) {
+    }
 }
 
